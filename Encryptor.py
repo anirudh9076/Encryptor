2362fb4453b0173110549ffe4aec9e0adcdcad83
anirudh9076 <65230997+anirudh9076@users.noreply.github.com>
2020-05-15 17:26:07 +0530

    Update Encryptor.py

--- Encryptor.py
+++ Encryptor.py
@@ -1,8 +1,11 @@
-
+import tkinter
 from tkinter import *
+
 from tkinter import filedialog
 
 path=''
+key=123
+root=Tk()
 
 def Encrypt_file():
 	wp=Tk()
@@ -15,33 +18,38 @@ def Encrypt_file():
 		path=filedialog.askopenfile()
 		if path!='':
 			l1=Label(wp,text="File Choosen and ready to be encrypted",bg='yellow').place(x=100,y=80)
-			l2=Label(wp,text="Enter Encyption key").place(x=100,y=120)
-			e_key=Entry(wp,width=50).place(x=230,y=120)
-			b_encrypt=Button(wp,text="Encrypt",bg='red',command=encryptor).place(x=150,y=150)
+			
 	def encryptor():
 		global path
+		global key
+		key=int(e_key.get())
 
 		f=open(path.name,'rb')
 		file=f.read()
 		f.close()
 		file=bytearray(file)
-		key=123
 		for index , value in enumerate(file):
 			file[index]=value^key
 		f1=open('encrypted.jpg','wb')
 		f1.write(file)
 		f1.close()
+
 	
-			
+	l2=Label(wp,text="Enter Encyption key").place(x=100,y=120)
+	b_encrypt=Button(wp,text="Encrypt",bg='red',command=encryptor).place(x=150,y=150)
+	e_key=Entry(wp,width=50)
+	e_key.place(x=230,y=120)		
+	
 	wp.config(bg='grey')
 	b_open=Button(wp,text="choose file",width=30,command=choosefile).place(x=100,y=50)
+	b_back=Button(wp,text="Back",width=30,bg='red',command=wp.destroy).place(x=200,y=500)
 	wp.mainloop()
 
 
-def decrypt_file():
+def Decrypt_file():
 	wp=Tk()
-	wp.geometry('600x600')
-	wp.title("File Encryptor")
+	wp.geometry('500x600')
+	wp.title("File Decryptor")
 
 
 	def choosefile():
@@ -49,30 +57,37 @@ def decrypt_file():
 		path=filedialog.askopenfile()
 		if path!='':
 			l1=Label(wp,text="File Choosen and ready to be encrypted",bg='yellow').place(x=100,y=80)
-			l2=Label(wp,text="Enter Encyption key").place(x=100,y=120)
-			e_key=Entry(wp,width=50).place(x=230,y=120)
-			b_encrypt=Button(wp,text="Decrypt",bg='red',command=decryptor).place(x=150,y=150)
-	
+			
+			
+
+
 	def decryptor():
 		global path
-
+		global key
+		key=int(e_key.get())
 		f=open(path.name,'rb')
 		file=f.read()
 		f.close()
 		file=bytearray(file)
-		key=123
+		
 		for index , value in enumerate(file):
 			file[index]=value^key
 		f1=open('decrypted.jpg','wb')
 		f1.write(file)
 		f1.close()
+	
+	l2=Label(wp,text="Enter Encyption key").place(x=100,y=120)
+	b_encrypt=Button(wp,text="Decrypt",bg='red',command=decryptor).place(x=150,y=150)		
+	e_key=Entry(wp,width=50)
+	e_key.place(x=230,y=120)
 			
 	wp.config(bg='grey')
 	b_open=Button(wp,text="choose file",width=30,command=choosefile).place(x=100,y=50)
 	wp.mainloop()
 
-root=Tk()
-root.geometry('600x600')
+
+
+root.geometry('500x600')
 b_file=Button(root,text="Encrypt file",bg='sky blue',fg='white',command=Encrypt_file).place(x=120,y=120)
-b_file=Button(root,text="decrypt file",bg='sky blue',fg='white',command=decrypt_file).place(x=120,y=160)
+b_file=Button(root,text="decrypt file",bg='sky blue',fg='white',command=Decrypt_file).place(x=120,y=160)
 root.mainloop()
