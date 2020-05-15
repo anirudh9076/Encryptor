5ccb3dc5f46f9e1d5b86ae31e9c6ebbb2d20c04d
Himanshu-dz <65234189+Himanshu-dz@users.noreply.github.com>
2020-05-15 19:09:13 +0700

    Update Encryptor.py

--- Encryptor.py
+++ Encryptor.py
@@ -3,7 +3,10 @@ from tkinter import *
 from tkinter import filedialog
 
 path=''
+key=''
 
+def exitt():
+	exit()
 def Encrypt_file():
 	wp=Tk()
 	wp.geometry('600x600')
@@ -12,20 +15,23 @@ def Encrypt_file():
 
 	def choosefile():
 		global path
+		global key
 		path=filedialog.askopenfile()
 		if path!='':
 			l1=Label(wp,text="File Choosen and ready to be encrypted",bg='yellow').place(x=100,y=80)
 			l2=Label(wp,text="Enter Encyption key").place(x=100,y=120)
-			e_key=Entry(wp,width=50).place(x=230,y=120)
+			e_key=Entry(wp,textvar=key,width=50).place(x=230,y=120)
 			b_encrypt=Button(wp,text="Encrypt",bg='red',command=encryptor).place(x=150,y=150)
 	def encryptor():
 		global path
-
+                global key
 		f=open(path.name,'rb')
 		file=f.read()
 		f.close()
 		file=bytearray(file)
-		key=123
+                key=int(key)
+		s=lambda key:0 if key==0 else key%10+s(key//10)
+		key=s(key)
 		for index , value in enumerate(file):
 			file[index]=value^key
 		f1=open('encrypted.jpg','wb')
@@ -35,6 +41,7 @@ def Encrypt_file():
 			
 	wp.config(bg='grey')
 	b_open=Button(wp,text="choose file",width=30,command=choosefile).place(x=100,y=50)
+	b_exit=Button(wp,text="Exit",relief='solid',fg='black',command=exitt).place(x=480,y=50)
 	wp.mainloop()
 
 
@@ -46,21 +53,24 @@ def decrypt_file():
 
 	def choosefile():
 		global path
+		global key
 		path=filedialog.askopenfile()
 		if path!='':
 			l1=Label(wp,text="File Choosen and ready to be encrypted",bg='yellow').place(x=100,y=80)
 			l2=Label(wp,text="Enter Encyption key").place(x=100,y=120)
-			e_key=Entry(wp,width=50).place(x=230,y=120)
+			e_key=Entry(wp,textvar=key,width=50).place(x=230,y=120)
 			b_encrypt=Button(wp,text="Decrypt",bg='red',command=decryptor).place(x=150,y=150)
 	
 	def decryptor():
 		global path
-
+                global key
 		f=open(path.name,'rb')
 		file=f.read()
 		f.close()
 		file=bytearray(file)
-		key=123
+                key=int(key)
+		s=lambda key:0 if key==0 else key%10+s(key//10)
+		key=s(key)
 		for index , value in enumerate(file):
 			file[index]=value^key
 		f1=open('decrypted.jpg','wb')
@@ -69,6 +79,7 @@ def decrypt_file():
 			
 	wp.config(bg='grey')
 	b_open=Button(wp,text="choose file",width=30,command=choosefile).place(x=100,y=50)
+	b_exit=Button(wp,text="Exit",relief='solid',fg='black',command=exitt).place(x=480,y=50)
 	wp.mainloop()
 
 root=Tk()
