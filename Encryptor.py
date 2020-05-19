1e6abfb63e6eb39d3a78eea4b49a9366806e5ede
anirudh9076 <65230997+anirudh9076@users.noreply.github.com>
2020-05-20 01:28:32 +0530

    get attachment added

--- Encryptor.py
+++ Encryptor.py
@@ -8,6 +8,7 @@ from tkinter import messagebox
 from email.mime.base import MIMEBase
 from email import encoders
 import os.path
+import imaplib, email, os
 
 path='' 	#to make the path accessible in any function
 key=int()   # to make the accessible in every function
@@ -191,11 +192,83 @@ def Emailing():
 	wm.mainloop()
 
 
+def getmail():
+	wm=Tk() # window to get encrypyed attachment and decrypt them
+	
+
+	def readmail():
+		user=e1.get()
+		password=e2.get()
+		imap_url = 'imap.gmail.com'
+		attachment_dir = 'H:\Programing\Encryptor'
+		# sets up the auth
+		def auth(user,password,imap_url):
+			con = imaplib.IMAP4_SSL(imap_url)
+			con.login(user,password)
+			return con
+
+		def get_attachments(msg):
+		    for part in msg.walk():
+		        if part.get_content_maintype()=='multipart':
+		            continue
+		        if part.get('Content-Disposition') is None:
+		            continue
+		        fileName = part.get_filename()
+
+		        if bool(fileName):
+		            filePath = os.path.join(attachment_dir, fileName)
+		            with open(filePath,'wb') as f:
+		                f.write(part.get_payload(decode=True))
+		    
+		    
+		    key=int(e3.get())
+		    f=open(fileName,'rb')
+		    file=f.read()
+		    f.close()
+		    file=bytearray(file)
+		    for index ,value in enumerate(file):
+		        file[index]=value^key
+		    f1=open(fileName,'wb')
+		    f1.write(file)
+		    f1.close()
+
+
+		con = auth(user,password,imap_url)
+		con.select('INBOX')
+
+		result, data = con.fetch(b'5','(RFC822)')
+		raw = email.message_from_bytes(data[0][1])
+		get_attachments(raw)
+
+	
+
+
+	
+	l=Label(wm,text='Welcome to encypted mailing',font='arial',fg='blue').place(x=150,y=50)
+	l1=Label(wm,text='Username:').place(x=50,y=100)
+	e1=Entry(wm,width=40)
+	e1.place(x=150,y=100)
+	l2=Label(wm,text='Password:').place(x=50,y=150)
+	e2=Entry(wm,width=40)
+	e2.place(x=150,y=150)
+	l3=Label(wm,text='Enter Key:').place(x=50,y=470)
+	e3=Entry(wm,width=60)
+	e3.place(x=150,y=470)
+	b=Button(wm,text='Get',fg='dark green',bg='light green',command=readmail)
+	b.place(x=250,y=550)
+	wm.title("Encrypted Email Terminal")
+	wm.geometry('500x600')
+	wm.mainloop()
+
+
+
+
 
 root.geometry('500x450')
 b_encrypt=Button(root,text="Encrypt file",bg='sky blue',fg='black',command=Encrypt_file).place(x=120,y=120)
 b_decrypt=Button(root,text="decrypt file",bg='sky blue',fg='black',command=Decrypt_file).place(x=120,y=160)
-b_email=Button(root,text="Emailing",bg='light green',fg='black',command=Emailing).place(x=120,y=200)
+b_Sendmail=Button(root,text="Send Mail",bg='light green',fg='black',command=Emailing).place(x=120,y=200)
+b_Getmail=Button(root,text="Get Mail",bg='light green',fg='black',command=getmail).place(x=120,y=240)
 quite_button=Button(root,text="Quit",bg='red',fg='black',command=root.quit).place(x=400,y=350)
 
 Labe_2=Label(root,text="Choose one option below:",fg="Blue").place(x=50,y=100)
